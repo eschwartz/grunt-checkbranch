088d846431cd425ffc5844fd5afc6766e84ab158
Edan Schwartz <eschwartz@weathernation.net>
2014-03-17 13:03:58 -0500

    Refactor/clean up checkbranch task
    - Name variables and methods descriptively
    - Template message so they are easier to read
    - Process default options
    - Move var declarations to start of scope
    - Whitespace cleanup

--- tasks/checkbranch.js
+++ tasks/checkbranch.js
@@ -12,35 +12,51 @@ var shell = require("shelljs");
 
 module.exports = function (grunt) {
 
-	grunt.registerTask('checkbranch', 'Check that we are on a correct Git branch before proceeding.', function (expectedBranch, force) {
+  function getCurrentBranch() {
+    var branchOutput = shell.exec("git rev-parse --abbrev-ref HEAD", {silent: true});
+    if (branchOutput.code !== 0) {
+      grunt.fail.fatal("Failed to detect the current branch");
+    }
 
-		if (grunt.option('no-checkbranch') && !force) {
-			grunt.log.writeln("Branch check overridden via command line.");
-			return;
-		}
+    return branchOutput.output.trim();
+  }
 
-		expectedBranch = expectedBranch || "master";
-		var negate = false;
-		if(expectedBranch[0] === "!") {
-			negate = true;
-			expectedBranch = expectedBranch.slice(1);
-		}
+  grunt.registerTask('checkbranch', 'Check that we are on a correct Git branch before proceeding.', function(expectedBranch, force) {
+    var isCorrectBranch;
+    var options = this.options({
+      force: force,
+      expectedBranch: expectedBranch || "master"
+    });
+    var isDisabled = grunt.option('no-checkbranch') && !options.force;
+    var isNegated = options.expectedBranch[0] === "!";
+    var currentBranch = getCurrentBranch();
 
-		grunt.log.writeln("Expecting to " + (negate ? "not" : "") + " be on '" + expectedBranch + "' branch.");
 
-		var branchOutput = shell.exec("git rev-parse --abbrev-ref HEAD", {silent: true});
-		if (branchOutput.code !== 0) {
-			grunt.fail.fatal("Failed to detect the current branch");
-		}
+    function template(str) {
+      return str.
+        replace("{expectedBranch}", expectedBranch).
+        replace("{currentBranch}", currentBranch).
+        replace("{not?}", isNegated ? " not" : "");
+    }
 
-		var branch = branchOutput.output.trim();
-		if (!negate && branch !== expectedBranch) {
-			grunt.fail.fatal("Only '"+expectedBranch+"' branch is allowed, and you're on '" + branch + "' branch.");
-		}
-		else if(negate && branch === expectedBranch) {
-			grunt.fail.fatal("Anything except '"+expectedBranch+"' branch is allowed, and you're on '" + branch + "' branch.");
-		}
+    expectedBranch = isNegated ? options.expectedBranch.slice(1) : options.expectedBranch;
+    isCorrectBranch = isNegated ? currentBranch !== expectedBranch : currentBranch === expectedBranch;
 
-	});
+    if (isDisabled) {
+      grunt.log.writeln("Branch check overridden via command line.");
+      return;
+    }
+
+    grunt.log.writeln(template("Expecting to{not?} be on {expectedBranch} branch"));
+
+    if (!isCorrectBranch) {
+      grunt.fail.fatal(template(
+        isNegated ?
+          "Anything except '{expectedBranch}' branch is allowed, and you're on '{currentBranch}' branch." :
+          "Only '{expectedBranch}' is allowed, and you're on '{currentBranch}' branch"
+      ));
+    }
+
+  });
 
 };
